1329db99932a15a1d810cadfd5c9ce500fbb31f7
Your Name <you@example.com>
2020-06-22 20:10:35 -0400

    Added style for focused section, fixed warnings

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,62 @@
 import React from 'react';
 import Styles from './Header.module.css';
 
-function Header({ changeFocus }) {
-    return(
-        <header>
-            <nav>
-                {<ul>
-                    <li><a onClick={changeFocus} href="#">World</a></li>
-                    <li><a onClick={changeFocus} href="#">National</a></li>
-                    <li><a onClick={changeFocus} href="#">Politics</a></li>
-                    <li><a onClick={changeFocus} href="#">Science</a></li>
-                    <li><a onClick={changeFocus} href="#">Sports</a></li>
-                </ul>}
-            </nav>
-        </header>
-    );
+function Header({ focus, changeFocus }) {
+  return (
+    <header>
+      <nav>
+        {
+          <ul>
+            <li>
+              <a
+                className={focus === 'world' ? Styles.focus : null}
+                onClick={changeFocus}
+                href='#0'
+              >
+                World
+              </a>
+            </li>
+            <li>
+              <a
+                className={focus === 'national' ? Styles.focus : null}
+                onClick={changeFocus}
+                href='#0'
+              >
+                National
+              </a>
+            </li>
+            <li>
+              <a
+                className={focus === 'politics' ? Styles.focus : null}
+                onClick={changeFocus}
+                href='#0'
+              >
+                Politics
+              </a>
+            </li>
+            <li>
+              <a
+                className={focus === 'science' ? Styles.focus : null}
+                onClick={changeFocus}
+                href='#0'
+              >
+                Science
+              </a>
+            </li>
+            <li>
+              <a
+                className={focus === 'sports' ? Styles.focus : null}
+                onClick={changeFocus}
+                href='#0'
+              >
+                Sports
+              </a>
+            </li>
+          </ul>
+        }
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
